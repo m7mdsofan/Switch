06c7716b80c96b1ccb9208b299b4656ea147b639
m7mdsofan <nash2t@hotmail.com>
2018-07-29 08:28:11 +0200

    Check and unChecked
    This is the last update 29/7/2018

---
+++ Switch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string HeSaid = "Hello";
+
+            switch (HeSaid)
+            {
+                case "Hello":
+                    Console.WriteLine("He Said Hello");
+                    break;
+                case "Bye":
+                    Console.WriteLine("He Said Bye");
+                    break;
+                case "Yes":
+                    Console.WriteLine("He Said Yes");
+                    break;
+
+                default:
+
+                    Console.WriteLine("He didn't say anything");
+                    break;
+            }
+         
+
+              
+                      
+                
+          
+          
+            
+
+Console.ReadKey();
+
+        }
+    }
+    }
+}
